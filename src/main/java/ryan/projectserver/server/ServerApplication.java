65f76196ed556c6defe8403ce0740b2017a6cb8c
Ryan <tankengwise1@gmail.com>
2024-02-06 07:59:00 +0100

    first

---
+++ src/main/java/ryan/projectserver/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package ryan.projectserver.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
