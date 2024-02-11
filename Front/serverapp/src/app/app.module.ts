89d947a4da34c7461333094e00e94e4eede998c2
Ryan <tankengwise1@gmail.com>
2024-02-11 19:25:03 +0100

    Creation de la table et ajout des données

--- Front/serverapp/src/app/app.module.ts
+++ Front/serverapp/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common'; 
+
+// 2h01 de la video il faut le style css
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
