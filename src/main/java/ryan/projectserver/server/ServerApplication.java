580815a28b3fcc09b14deb579b72034e9fbeb1c2
Ryan <tankengwise1@gmail.com>
2024-02-11 18:10:51 +0100

    suppression

--- src/main/java/ryan/projectserver/server/ServerApplication.java
+++ src/main/java/ryan/projectserver/server/ServerApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import ryan.projectserver.server.enumeration.Status;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import ryan.projectserver.server.model.Server;
 import ryan.projectserver.server.repo.ServerRepo;
+import java.util.Arrays;
 
 import static ryan.projectserver.server.enumeration.Status.SERVER_DOWN;
 import static ryan.projectserver.server.enumeration.Status.SERVER_UP;
@@ -30,5 +33,21 @@ public class ServerApplication {
 			serverRepo.save(new Server(null, "192.168.1.64", "M5 2008", "128 GB", "Web Server",
 					"http://localhost:8080/server/images/server4.png", SERVER_DOWN));
 		};
+	}
+
+	@Bean
+	public CorsFilter corsFilter(){
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
+				"Access-Control-Allow-Origin","Access-Control-Allow-Origin","Access-Control-Allow-Credentials", "Filename"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+	 	return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
 }
--- src/main/java/ryan/projectserver/server/resource/ServerResource.java
+++ src/main/java/ryan/projectserver/server/resource/ServerResource.java
@@ -13,6 +13,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 
 import static java.time.LocalDateTime.now;
 import static java.util.Map.of;
@@ -28,7 +29,8 @@ public class ServerResource {
     private final ServerServiceImpl serverService;
 
     @GetMapping("/list")
-    public ResponseEntity<Response> getServers(){
+    public ResponseEntity<Response> getServers() throws InterruptedException{
+        //TimeUnit.SECONDS.sleep(3);
         return ResponseEntity.ok(
                 Response.builder()
                         .timeStamp(now())
